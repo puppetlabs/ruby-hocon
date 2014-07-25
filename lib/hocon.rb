702146cc2ff7154c1d193adef6943da978bd19a9
Wayne <wayne@puppetlabs.com>
2014-07-24 18:29:00 -0700

    Hocon: Fix spec tests by adding `load` and `parse`
    Signed-off-by: Wayne <wayne@puppetlabs.com>

--- lib/hocon.rb
+++ lib/hocon.rb
@@ -1,2 +1,13 @@
+require 'hocon/config_factory'
+
 module Hocon
+  def self.load(file)
+    config = Hocon::ConfigFactory.parse_file(file)
+    return config.root.unwrapped
+  end
+
+  def self.parse(string)
+    config = Hocon::ConfigFactory.parse_string(string)
+    return config.root.unwrapped
+  end
 end
