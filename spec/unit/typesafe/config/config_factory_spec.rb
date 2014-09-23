9339f6066504bbb2021f21c53077a6aeb41e5bc9
Preben Ingvaldsen <preben@puppetlabs.com>
2014-09-23 16:30:36 -0700

    Put get_value tests into their own file
    Move the tests of the SimpleConfig get_value method into a new
    file, simple_config_spec.rb

--- spec/unit/typesafe/config/config_factory_spec.rb
+++ spec/unit/typesafe/config/config_factory_spec.rb
@@ -30,58 +30,33 @@ describe Hocon::ConfigFactory do
     end
   end
 
-  shared_examples_for "config_value_retrieval_single_value" do
-    it "should allow you to get a value for a specific configuration setting" do
-      expect(conf.get_value(setting).transform_to_string).to eq(expected_setting)
-    end
-  end
-
-  shared_examples_for "config_value_retrieval_config_list" do
-    it "should allow you to get a value for a setting whose value is a data structure" do
-      expect(conf.get_value(setting).
-                 render_value_to_sb(StringIO.new, 2, nil,
-                                    Hocon::ConfigRenderOptions.new(false, false, false, false)).
-                 string).to eq(expected_setting)
-    end
-  end
-
-
-
   context "example1" do
     let(:example) { EXAMPLE1 }
-    let(:setting) { "foo.bar.yahoo" }
-    let (:expected_setting) { "yippee" }
 
     context "parsing a HOCON string" do
       let(:string) { File.open(input_file).read }
       let(:conf) { Hocon::ConfigFactory.parse_string(string) }
       include_examples "config_factory_parsing"
-      include_examples "config_value_retrieval_single_value"
     end
 
     context "parsing a .conf file" do
       let(:conf) { Hocon::ConfigFactory.parse_file(input_file) }
       include_examples "config_factory_parsing"
-      include_examples "config_value_retrieval_single_value"
     end
   end
 
   context "example2" do
     let(:example) { EXAMPLE2 }
-    let(:setting) { "jruby-puppet.jruby-pools" }
-    let (:expected_setting) { "[{environment=production}]" }
 
     context "parsing a HOCON string" do
       let(:string) { File.open(input_file).read }
       let(:conf) { Hocon::ConfigFactory.parse_string(string) }
       include_examples "config_factory_parsing"
-      include_examples "config_value_retrieval_config_list"
     end
 
     context "parsing a .conf file" do
       let(:conf) { Hocon::ConfigFactory.parse_file(input_file) }
       include_examples "config_factory_parsing"
-      include_examples "config_value_retrieval_config_list"
     end
   end
 end
