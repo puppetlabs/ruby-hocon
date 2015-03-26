4e389d3b3cfe483ac174adea9153640012122b78
Chris Price <chris@puppetlabs.com>
2015-03-25 23:03:28 -0700

    (MAINT) Sync ConfigDelayedMerge

--- lib/hocon/impl/config_delayed_merge.rb
+++ lib/hocon/impl/config_delayed_merge.rb
@@ -1,10 +1,21 @@
 require 'hocon/impl'
 require 'hocon/impl/replaceable_merge_stack'
 require 'hocon/impl/config_delayed_merge_object'
+require 'hocon/impl/config_impl'
 
+#
+# The issue here is that we want to first merge our stack of config files, and
+# then we want to evaluate substitutions. But if two substitutions both expand
+# to an object, we might need to merge those two objects. Thus, we can't ever
+# "override" a substitution when we do a merge; instead we have to save the
+# stack of values that should be merged, and resolve the merge when we evaluate
+# substitutions.
+#
 class Hocon::Impl::ConfigDelayedMerge < Hocon::Impl::AbstractConfigValue
   include Hocon::Impl::Unmergeable
   include Hocon::Impl::ReplaceableMergeStack
+
+  ConfigImpl = Hocon::Impl::ConfigImpl
 
   def initialize(origin, stack)
     super(origin)
@@ -35,8 +46,187 @@ class Hocon::Impl::ConfigDelayedMerge < Hocon::Impl::AbstractConfigValue
     raise Hocon::ConfigError::ConfigNotResolvedError.new(error_message, nil)
   end
 
+  def resolve_substitution(context, source)
+    self.class.resolve_substitution(self, stack, context, source)
+  end
+
+  def self.resolve_substitution(context, source, replaceable = self, stack = @stack)
+    if ConfigImpl.trace_substitution_enabled
+      ConfigImpl.trace("delayed merge stack has #{stack.size} items:", context.depth)
+      count = 0
+      stack.each do |v|
+        ConfigImpl.trace("#{count}: #{v}", context.depth)
+        count += 1
+      end
+    end
+
+    # to resolve substitutions, we need to recursively resolve
+    # the stack of stuff to merge, and merge the stack so
+    # we won't be a delayed merge anymore. If restrictToChildOrNull
+    # is non-null, or resolve options allow partial resolves,
+    # we may remain a delayed merge though.
+
+    new_context = context
+    count = 0
+    merged = nil
+    stack.each do |stack_end|
+      # the end value may or may not be resolved already
+
+      source_for_end = nil
+
+      if stack_end.is_a?(ReplaceableMergeStack)
+        raise ConfigBugOrBrokenError, "A delayed merge should not contain another one: #{replaceable}"
+      elsif stack_end.is_a?(Unmergeable)
+        # the remainder could be any kind of value, including another
+        # ConfigDelayedMerge
+        remainder = replaceable.make_replacement(context, count + 1)
+
+        if ConfigImpl.trace_substitution_enabled
+          ConfigImpl.trace("remainder portion: #{remainder}", new_context.depth)
+        end
+
+        # If, while resolving 'end' we come back to the same
+        # merge stack, we only want to look _below_ 'end'
+        # in the stack. So we arrange to replace the
+        # ConfigDelayedMerge with a value that is only
+        # the remainder of the stack below this one.
+
+        if ConfigImpl.trace_substitution_enabled
+          ConfigImpl.trace("building sourceForEnd", new_context.depth)
+        end
+
+        # we resetParents() here because we'll be resolving "end"
+        # against a root which does NOT contain "end"
+        source_for_end = source.replace_within_current_parent(replaceable, remainder)
+
+        if ConfigImpl.trace_substitution_enabled
+          ConfigImpl.trace("  sourceForEnd before reset parents but after replace: #{source_for_end}", new_context.depth)
+        end
+
+        source_for_end = source_for_end.reset_parents
+      else
+        if ConfigImpl.trace_substitution_enabled
+          ConfigImpl.trace("will resolve end against the original source with parent pushed",
+                           new_context.depth)
+        end
+
+        source_for_end = source.push_parent(replaceable)
+      end
+
+      if ConfigImpl.trace_substitution_enabled
+        ConfigImpl.trace("sourceForEnd      =#{source_for_end}", new_context.depth)
+      end
+
+      if ConfigImpl.trace_substitution_enabled
+        ConfigImpl.trace("Resolving highest-priority item in delayed merge #{stack_end}" +
+                          " against #{source_for_end} endWasRemoved=#{(source != sourceForEnd)}")
+      end
+
+      result = new_context.resolve(stack_end, source_for_end)
+      resolved_end = result.value
+      new_context = result.context
+
+      if ! resolved_end.nil?
+        if merged.nil?
+          merged = resolved_end
+        else
+          if ConfigImpl.trace_substitution_enabled
+            ConfigImpl.trace("merging #{merged} with fallback #{resolvedEnd}",
+                             new_context.depth + 1)
+          end
+          merged = merged.with_fallback(resolved_end)
+        end
+      end
+
+      count += 1
+
+      if ConfigImpl.trace_substitution_enabled
+        ConfigImpl.trace("stack merged, yielding: #{merged}",
+                         new_context.depth)
+      end
+    end
+
+    ResolveResult.make(new_context, merged)
+  end
+
+  def make_replacement(context, skipping)
+    self.class.make_replacement(context, stack, skipping)
+  end
+
+  # static method also used by ConfigDelayedMergeObject; end may be null
+  def self.make_replacement(context, stack, skipping)
+    sub_stack = stack.sub_list(skipping, stack.size)
+
+    if sub_stack.empty?
+      if ConfigImpl.trace_substitution_enabled
+        ConfigImpl.trace("Nothing else in the merge stack, replacing with null", context.depth)
+        nil
+      else
+        # generate a new merge stack from only the remaining items
+        merged = nil
+        sub_stack.each do |v|
+          if merged.nil?
+            merged = v
+          else
+            merged = merged.with_fallback(v)
+          end
+        end
+        merged
+      end
+    end
+
+  end
+
   def resolve_status
     Hocon::Impl::ResolveStatus::UNRESOLVED
+  end
+
+  def replace_child(child, replacement)
+    new_stack = replace_child_in_list(stack, child, replacement)
+    if new_stack.nil?
+      nil
+    else
+      self.class.new(origin, new_stack)
+    end
+  end
+
+  def has_descendant(descendant)
+    has_descendant_in_list(stack, descendant)
+  end
+
+  def relativized(prefix)
+    new_stack = stack.map { |o| o.relativized(prefix) }
+    self.class.new(origin, new_stack)
+  end
+
+  # static utility shared with ConfigDelayedMergeObject
+  def self.stack_ignores_fallbacks(stack)
+    last = stack[-1]
+    last.ignores_fallbacks
+  end
+
+  def ignores_fallbacks
+    self.class.stack_ignores_fallbacks(stack)
+  end
+
+  def new_copy(new_origin)
+    self.class.new(new_origin, stack)
+  end
+
+  def merged_with_the_unmergeable(fallback)
+    merged_stack_with_the_unmergeabls(stack, fallback)
+  end
+
+  def merged_with_object(fallback)
+    merged_stack_with_object(stack, fallback)
+  end
+
+  def merged_with_non_object(fallback)
+    merged_stack_with_non_object(stack, fallback)
+  end
+
+  def unmerged_values
+    stack
   end
 
   def can_equal(other)
@@ -131,4 +321,5 @@ class Hocon::Impl::ConfigDelayedMerge < Hocon::Impl::AbstractConfigValue
       sb << "# ) end of unresolved merge\n"
     end
   end
+
 end
