391d1c890fb7212777b099cd609dd0242ed45356
Preben Ingvaldsen <preben@puppetlabs.com>
2015-04-06 16:07:27 -0700

    (TK-186) Update comment on AbstractConfigNodeValue
    Update the comment on the AbstractConfigNodeValue to reflect that
    the module is unnecessary in Ruby and is being preserved solely
    for consistency.

--- lib/hocon/impl/abstract_config_node_value.rb
+++ lib/hocon/impl/abstract_config_node_value.rb
@@ -3,9 +3,9 @@
 require 'hocon/impl'
 require 'hocon/impl/abstract_config_node'
 
-# This is required if we want
-# to be referencing the AbstractConfigNode class in implementation rather than the
-# ConfigNode interface, as we can't cast an AbstractConfigNode to an interface
+# This essentially exists in the upstream so we can ensure only certain types of
+# config nodes can be passed into some methods. That's not a problem in Ruby, so this is
+# unnecessary, but it seems best to keep it around for consistency
 module Hocon::Impl::AbstractConfigNodeValue
   include Hocon::Impl::AbstractConfigNode
 end
