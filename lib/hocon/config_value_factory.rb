89f57cc1688408c68bb3fc88217f47b116fbce05
Preben Ingvaldsen <preben@puppetlabs.com>
2015-07-15 15:27:28 -0700

    (TK-251) Convert symbol keys to strings
    When parsing a Hash in ConfigValueFactory, automatically convert
    all symbol keys to strings.

--- lib/hocon/config_value_factory.rb
+++ lib/hocon/config_value_factory.rb
@@ -55,7 +55,11 @@ class Hocon::ConfigValueFactory
   # @return a new value
   #
   def self.from_any_ref(object, origin_description = nil)
-    ConfigImpl.from_any_ref(object, origin_description)
+    if object.is_a?(Hash)
+      from_map(object, origin_description)
+    else
+      ConfigImpl.from_any_ref(object, origin_description)
+    end
   end
 
   #
@@ -69,6 +73,7 @@ class Hocon::ConfigValueFactory
   # @return a new {@link ConfigObject}
   #
   def self.from_map(values, origin_description = nil)
-    ConfigImpl.from_any_ref(values, origin_description)
+    processed_hash = Hash[values.map {|k, v| [k.is_a?(Symbol) ? k.to_s : k, v]}]
+    ConfigImpl.from_any_ref(processed_hash, origin_description)
   end
 end
