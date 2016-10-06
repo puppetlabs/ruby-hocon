846040d02664d12dccfb65ce9c5e0d823a714418
Joe Pinsonault <joe.pinsonault@gmail.com>
2016-10-06 16:31:13 -0700

    (HC-92) Improve modularity

--- lib/hocon/cli.rb
+++ lib/hocon/cli.rb
@@ -76,14 +76,16 @@ module Hocon::CLI
   # Main entry point into the script
   # Calls the appropriate subcommand
   def self.main(opts)
+    hocon_text = get_hocon_file(opts[:in_file])
+
     begin
       case opts[:subcommand]
         when 'get'
-          puts do_get(opts)
+          puts do_get(opts, hocon_text)
         when 'set'
-          print_or_write(do_set(opts), opts[:out_file])
+          print_or_write(do_set(opts, hocon_text), opts[:out_file])
         when 'unset'
-          print_or_write(do_unset(opts), opts[:out_file])
+          print_or_write(do_unset(opts, hocon_text), opts[:out_file])
       end
 
     rescue ConfigMissingError, ConfigWrongTypeError
@@ -98,8 +100,8 @@ module Hocon::CLI
   # Entry point for the 'get' subcommand
   # Returns a string representation of the the value at the path given on the
   # command line
-  def self.do_get(opts)
-    config = get_hocon_config(opts)
+  def self.do_get(opts, hocon_text)
+    config = Hocon::ConfigFactory.parse_string(hocon_text)
     value = config.get_any_ref(opts[:path])
 
     render_options = Hocon::ConfigRenderOptions.defaults
@@ -114,8 +116,8 @@ module Hocon::CLI
   # Entry point for the 'set' subcommand
   # Returns a string representation of the HOCON config after adding/replacing
   # the value at the given path with the given value
-  def self.do_set(opts)
-    config_doc = get_hocon_doc(opts)
+  def self.do_set(opts, hocon_text)
+    config_doc = Hocon::Parser::ConfigDocumentFactory.parse_string(hocon_text)
     modified_config_doc = config_doc.set_value(opts[:path], opts[:new_value])
 
     modified_config_doc.render
@@ -124,37 +126,19 @@ module Hocon::CLI
   # Entry point for the 'unset' subcommand
   # Returns a string representation of the HOCON config after removing the
   # value at the given path
-  def self.do_unset(opts)
-    config_doc = get_hocon_doc(opts)
+  def self.do_unset(opts, hocon_text)
+    config_doc = Hocon::Parser::ConfigDocumentFactory.parse_string(hocon_text)
     modified_config_doc = config_doc.remove_value(opts[:path])
 
     modified_config_doc.render
   end
 
-  # Returns a Config object from the file given on the command line
-  # or from STDIN if no file is specified
-  def self.get_hocon_config(opts)
-    if opts[:in_file]
-      file_path = File.expand_path(opts[:in_file])
-      config = Hocon::ConfigFactory.parse_file(file_path)
+  def self.get_hocon_file(in_file)
+    if in_file
+      File.read(in_file)
     else
-      config = Hocon::ConfigFactory.parse_string(STDIN.read)
+      STDIN.read
     end
-
-    config
-  end
-
-  # Returns a ConfigDocument object from the file given on the command line
-  # or from STDIN if no file is specified
-  def self.get_hocon_doc(opts)
-    if opts[:in_file]
-      file_path = File.expand_path(opts[:in_file])
-      config_doc = Hocon::Parser::ConfigDocumentFactory.parse_file(file_path)
-    else
-      config_doc = Hocon::Parser::ConfigDocumentFactory.parse_string(STDIN.read)
-    end
-
-    config_doc
   end
 
   # Print an error message and exit the program
